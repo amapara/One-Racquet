5ee2060934fe9d5399585be4c525543943965a63
Alpesh Mapara <alpesh.m@btinternet.com>
2019-06-10 14:37:31 -0400

    added has_many and belongs_to

---
+++ db/migrate/20190610182415_create_offers.rb
@@ -0,0 +1,13 @@
+class CreateOffers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :offers do |t|
+      t.date :date
+      t.time :time
+      t.integer :length
+      t.references :user, foreign_key: true
+      t.references :court, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
