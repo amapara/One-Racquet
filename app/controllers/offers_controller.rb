56528a6b372b788ee14abe1af0ad83344c071169
Alastair Hewitt <alastairjohnhewitt@gmail.com>
2019-06-10 17:47:56 -0300

    a little unsure of what to add, please review

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,15 @@
 class OffersController < ApplicationController
+
+  def new
+    @offer = Offer.find(params[:id])
+  end
+
+  def create
+  end
+
+  private
+
+  def offer_params
+    params.require(:user).permit(:name, :skill, :booking)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/home', to: 'pages#home'
-  resources :offers, only: [:index]
+  resources :offers, only: [:index, :new]
 end
