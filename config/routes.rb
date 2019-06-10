3a60679bd58fc15ca0665017b40e03065ceaab01
Emily Weidt <emily.weidt@mail.de>
2019-06-10 17:01:55 -0400

    offers_index route creates. tested card indentation on viewer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  # resources :offers do
+  #   resources :booking
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/offers', to: 'offers#index', as: :offers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2019_06_10_182453) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "skill"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
