4b5277fa83201ff5febff1dc6725462226e9ce54
emilywe170694 <49698278+emilywe170694@users.noreply.github.com>
2019-06-10 17:28:21 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # resources :offers do
-  #   resources :booking
-  # end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/offers', to: 'offers#index', as: :offers
-  get 'pages/home', to: 'pages#home'
-  resources :offers, only: [:index, new]
+
+  resources :offers, only: [:index, :new]
 end
