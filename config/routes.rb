7886ba052058fa0aa83f1d15e0d9a80c62075f67
Alastair Hewitt <alastairjohnhewitt@gmail.com>
2019-06-10 17:06:23 -0300

    struggled to get date / time to clear.  Need to fix boxes & add search functionality to location

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/home', to: 'pages#home'
+  resources :offers, only: [:index]
 end
