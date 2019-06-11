99e6f390f7bad9f02c68430a7838aa31f1d31fe8
emilywe170694 <49698278+emilywe170694@users.noreply.github.com>
2019-06-11 11:20:22 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 
 require 'faker'
 require 'date'
 
-# def rand_int(from, to)
-#   rand_in_range(from, to).to_i
-# end
-
-# def rand_price(from, to)
-#   rand_in_range(from, to).round(2)
-# end
-
-# def rand_time(from, to=Time.now)
-#   Time.at(rand_in_range(from.to_f, to.to_f))
-# end
-
-# def rand_in_range(from, to)
-#   rand * (to - from) + from
-# end
 
 skill_sample = ["high","medium","low"]
 puts 'Creating 5 fake users...'
