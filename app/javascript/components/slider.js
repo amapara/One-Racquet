3cb6896acc3f6ed4e986654cce28887ac0a547b5
Alpesh Mapara <alpesh.m@btinternet.com>
2019-06-13 17:45:54 -0400

    added

--- app/javascript/components/slider.js
+++ app/javascript/components/slider.js
@@ -6,8 +6,13 @@ const initSlider = () => {
         grid: true,
         values: ["6:00","7:00","8:00","9:00", "10:00", "11:00",
                 "12:00", "13:00", "14:00", "15:00", "16:00", "17:00",
-                "18:00", "19:00"]
-        });
+                "18:00", "19:00"],
+        onFinish: function (data) {
+            console.log(data.from);
+            console.log(data.to);
+            // fired on pointer release
+        },
+    });
   }
 
 export {initSlider}
