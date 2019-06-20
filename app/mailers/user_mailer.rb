a40f1b454e4b9b1882735bc60d7c85e84623718a
Alastair Hewitt <alastairjohnhewitt@gmail.com>
2019-06-20 13:50:29 -0300

    some mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,12 @@ class UserMailer < ApplicationMailer
   def welcome
 
     @user = params[:user] # Instance variable => available in view
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    mail(to: @user.email, subject: 'Welcome to One Racquet')
     # This will render a view in `app/views/user_mailer`!
+  end
+
+  def booking
+    @booking = params[:booking]
+    mail(to: @user.email, subject: 'Booking made')
   end
 end
