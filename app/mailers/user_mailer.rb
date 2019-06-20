9ee124dbcdcb5b7151dca530f319b860f763e852
Alastair Hewitt <alastairjohnhewitt@gmail.com>
2019-06-20 12:32:21 -0300

    welcome email works

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,14 @@
 class UserMailer < ApplicationMailer
-    @user = params[:user]
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome.subject
+  #
+  def welcome
+
+    @user = params[:user] # Instance variable => available in view
+    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    # This will render a view in `app/views/user_mailer`!
   end
 end
